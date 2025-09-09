6552fbdfc11991d3e045eaa55743a64758ac3e6a
Vedant005 <vedantkanekar909@gmail.com>
2025-09-10 01:10:36 +0530

    chore: express app setup

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+export { app };
