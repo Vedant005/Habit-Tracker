87cdeba4f5a516d889e2be7fc9a9406f1456ed15
Vedant005 <vedantkanekar909@gmail.com>
2025-09-13 20:29:37 +0530

    feat: adding routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//imports
+
+import userRouter from "./routes/user.js"
+import habitRouter from "./routes/habit.js"
+import followRouter from "./routes/follow.js"
+import checkInRouter from "./routes/checkIn.js"
+
+
+app.use("/ap1/v1/users",userRouter);
+app.use("/ap1/v1/habit",habitRouter);
+app.use("/ap1/v1/follow",followRouter);
+app.use("/ap1/v1/checkIn",checkInRouter);
+
+
 app.get("/health", (req, res) => {
   res.send("OK");
 });
